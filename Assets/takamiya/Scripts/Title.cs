2f82e81db74d8f6c544f5619ebea30d7f0e22822
yuutotakamiya <231065@st.yoshida-g.ac.jp>
2025-09-30 14:35:41 +0900

    タイトルシーン追加、チュートリアルシーン追加

---
+++ Assets/takamiya/Scripts/Title.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Title : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartButton()
+    {
+        Initiate.Fade("Tutorial",Color.black,1.0f);
+    }
+}
